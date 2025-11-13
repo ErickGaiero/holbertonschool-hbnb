2726e84db4d58d9d2bf8bf4e6658df03559c6e9c
Nahuel Bica <nahuelbica5912@gmail.com>
2025-11-13 03:39:25 -0300

    - added requirements - edited config.py - edited app/__init

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_restx import Api
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
+from flask_sqlalchemy import SQLAlchemy
 from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
@@ -9,11 +10,13 @@ from app.api.v1.auth import api as auth_ns
 
 bcrypt = Bcrypt()
 jwt = JWTManager()
+db = SQLAlchemy()
 
 def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
     app.config.from_object(config_class)
-
+    db.init_app(app)
+    
     bcrypt.init_app(app)
     jwt.init_app(app) 
     
